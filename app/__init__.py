e2e8db5a94e5069d216a0062aa86bfcd158acd72
RollDack <marquesmatheus.psn@gmail.com>
2025-04-09 21:10:29 -0300

    Implementação de cadastro de clientes

--- app/__init__.py
+++ app/__init__.py
@@ -5,17 +5,19 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'sua_chave_secreta_aqui'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:password@localhost/optical_store'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
-
     from app.routes.home import home_bp
+    from app.routes.customer import customers_bp
     from app.routes.products import products_bp
     from app.routes.sales import sales_bp
 
     app.register_blueprint(home_bp)
+    app.register_blueprint(customers_bp, url_prefix='/customers')
     app.register_blueprint(products_bp, url_prefix='/products')
     app.register_blueprint(sales_bp, url_prefix='/sales')
 
--- app/models.py
+++ app/models.py
@@ -6,6 +6,15 @@ class Product(db.Model):
     price = db.Column(db.Float, nullable=False)
     stock = db.Column(db.Integer, nullable=False)
 
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    phone = db.Column(db.String(20))
+    birthdate = db.Column(db.Date)
+
+
 class Sale(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
